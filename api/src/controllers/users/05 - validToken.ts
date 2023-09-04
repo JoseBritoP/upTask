13cb0aa07cab8a9a1f42aac4de3beca78146bcce
JoseBritoP <josealbritose@gmail.com>
2023-09-04 04:18:33 -0300

    Type validToken

--- api/src/controllers/users/05 - validToken.ts
+++ api/src/controllers/users/05 - validToken.ts
@@ -1,6 +1,6 @@
 import User from "../../models/User";
 
-export const validTokenToChangePassword = async (token:string) => {
+export const validTokenToChangePassword = async (token:string):Promise<String> => {
   const user = await User.findOne({token});
   if(!user) throw new Error(`Token invalid`);
   return `Token valid`;
