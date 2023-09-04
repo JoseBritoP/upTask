02d3565cf4087ba657f41726fdb18012b21bea91
JoseBritoP <josealbritose@gmail.com>
2023-09-04 04:21:00 -0300

    Type newPassword

--- api/src/controllers/users/06 - newPassword.ts
+++ api/src/controllers/users/06 - newPassword.ts
@@ -1,21 +1,18 @@
 import User from "../../models/User";
 import { bycrypt } from "../../utils/bycript.handler";
-import { ChangePassword } from "../../types/user";
+import { ChangePassword,UserInterface } from "../../types/user";
 
-export const newPassword = async ({ token, password }: ChangePassword) => {
-  try {
-    const user = await User.findOne({ token });
+export const newPassword = async ({ token, password }: ChangePassword):Promise<String> => {
+    const user = await User.findOne({ token })
+    .select("_id username email")
 
     if (!user) throw new Error(`User not found`);
 
-
     const passwordHash = await bycrypt(password);
+
     user.password = passwordHash; 
     user.token = "";
     await user.save(); 
 
-    return user
-  } catch (error:any) {
-    throw new Error(error.message);
-  }
+    return `The password has been change successfully!`
 };
