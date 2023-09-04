69d64a54b14a9939b40a7a31e49b3fe961f0b2b3
JoseBritoP <josealbritose@gmail.com>
2023-09-04 04:17:00 -0300

    Confirm password

--- api/src/controllers/users/03 - confirmToken.ts
+++ api/src/controllers/users/03 - confirmToken.ts
@@ -1,6 +1,6 @@
 import User from "../../models/User";
 
-export const confirmToken = async (token:string) => {
+export const confirmToken = async (token:string):Promise<String> => {
   const user = await User.findOne({token})
   if(!user) throw new Error(`Token invalid`);
 
