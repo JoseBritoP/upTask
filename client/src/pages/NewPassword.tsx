5a7449b1d862d8ac701711697732d4d70085eef5
JoseBritoP <josealbritose@gmail.com>
2023-09-05 18:56:09 -0300

    new password

--- client/src/pages/NewPassword.tsx
+++ client/src/pages/NewPassword.tsx
@@ -21,36 +21,31 @@ const NewPassword = () => {
             {/* Password */}
             <div className="my-5">
               <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">New Password</label>
+
               <Field type="password" name="password" id="password" placeholder="Ingresa tu nueva contraseña" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" />
-              <ErrorMessage name='password' componet = {()=>(
+
+              <ErrorMessage name='password' component={() => (
                 <p className='text-red-500 font-semibold'>{errors.password}</p>
               )}/>
+
             </div>
             {/* Repeat Password */}
             <div className="my-5">
               <label htmlFor="repeatPassword" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Repeat Password</label>
+
               <Field 
-                type="password" name="repeatPassword" id="repeatPassword" placeholder="Ingresa tu contraseña password" 
+                type="password" name="repeatPassword" id="repeatPassword" placeholder="Confirma tu contraseña" 
                 className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
               />
+
               <ErrorMessage name="repeatPassword" component = {()=>(
                 <p className='text-red-500 font-semibold'>{errors.repeatPassword}</p>
               )}/>
+
             </div>
           </Form>
         )}
       </Formik>
-      {/* <form action="" className="my-10 bg-white shadow rounded-lg px-10 py-5">
-        <div className="my-5">
-          <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">New Password</label>
-          <input type="password" name="password" id="password" placeholder="Ingresa tu nueva contraseña" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" />
-        </div>
-        <div className="my-5">
-          <label htmlFor="repeat-password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Repetir password</label>
-          <input type="password" name="repeat-password" id="repeat-password" placeholder="Repite tu contraseña" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" />
-        </div>
-        <input type="submit" value="Guardar cambios" className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5" />
-      </form> */}
   </>
   )
 }
