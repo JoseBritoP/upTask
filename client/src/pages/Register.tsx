9114262d0e21eba898f394a97bb11e3e23041494
JoseBritoP <josealbritose@gmail.com>
2023-09-07 12:38:20 -0300

    Register scheme

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,166 +1,119 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Formik, Form, Field,ErrorMessage } from 'formik'
-import { validateFormRegister } from '../helpers/validate'
+import { useFormik } from 'formik'
+import { registerScheme } from '../scheme/RegisterScheme'
+import z from 'zod'
 
+type Register = z.infer<typeof registerScheme>
 const Register = () => {
+
   const [exit,setExit] = useState(false)
+  const formik = useFormik({
+    initialValues:{username:'',email:'',password:''},
+    onSubmit:(values)=>{
+      console.log(values)
+      setExit(true)
+    },
+
+    validate:(values)=>{
+      
+      const result = registerScheme.safeParse(values);
+      if(result.success) return;
+      const errors:Record<string,string> = {}
+      result.error.issues.forEach((error)=>{
+        errors[error.path[0]] = error.message
+      });
+      return errors
+    },
+    validateOnBlur:true
+  })
 
   return (
     <>
       <h1 className="text-sky-600 font-black text-4xl uppercase text-center">Crea tu cuenta {<br></br>} para administrar tus <span className="text-slate-700">proyectos</span>
       </h1>
-      <Formik 
-        initialValues={{
-          username: '',
-          email:'',
-          password:'',
-          repeatPassword:''
-        }}
-        // validate={(values)=>{
-        //   const errors = {
-        //     username: '',
-        //     email: '',
-        //     password: '',
-        //     repeatPassword:''
-        //   }
-        //   if(!values.username){
-        //     console.log('Por favor escribe un nombre')
-        //     errors.username = 'Por favor ingrese un apodo'
-        //   } else if(!/^[a-zA-ZÀ-ÿ\s]{5,20}$/.test(values.username)){
-        //     errors.username='Su apodo solo puede contener letras y tener más de 4 caracteres'
-        //   }
-        //   if(!values.email){
-        //     console.log('Por favor ingrese su correo')
-        //     errors.email = 'Por favor ingrese su correo'
-        //   } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)){
-        //     errors.email= "El formato del correo no es correcto"
-        //   }
-        //   if(!values.password){
-        //     console.log("Porfavor ingresa una contraseña")
-        //     errors.password= "Porfavor ingresa una contraseña"
-        //   }
-        //   if(!values.repeatPassword){
-        //     console.log("Porfavor repita su contraseña")
-        //     errors.repeatPassword="Porfavor repita su contraseña"
-        //   }
-        //   if(values.repeatPassword !== values.password){
-        //     console.log('Las contraseñas no coinciden')
-        //     errors.repeatPassword='Las contraseñas no coinciden'
-        //   }
-        //   return errors
-        // }}
-        onSubmit={(values,{resetForm})=>{
-          console.log(values)
-          console.log('Enviando formulario...')
-          setExit(true)
-          resetForm();
-        }}
-        validate={validateFormRegister}
+      <form
+        action=""
+        className="my-10 bg-white shadow rounded-lg px-10 py-5"
+        onSubmit={formik.handleSubmit}
       >
-        {({errors})=>(
-          <Form action="" className="my-10 bg-white shadow rounded-lg px-10 py-5">
-            {/* Username */}
-            <div className="my-5">
-              <label htmlFor="username" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Username</label>
-              <Field 
-                type="text" name="username" id="username" 
-                placeholder="Username" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-              />
-              <ErrorMessage name="username" component = {()=>(
-                <p className='text-red-500 font-semibold'>{errors.username}</p>
-              )}/>
-            </div>
-            {/* Email */}
-            <div className="my-5">
-              <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Email</label>
-              <Field 
-                type="email" name="email" id="email" placeholder="Email de registro"
-                className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-              />
-                <ErrorMessage name="email" component = {()=>(
-                <p className='text-red-500 font-semibold'>{errors.email}</p>
-              )}/>
-            </div>
-            {/* Password */}
-            <div className="my-5">
-              <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Password</label>
-              <Field 
-                type="password" name="password" id="password" placeholder="Ingresa tu contraseña password" 
-                className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-              />
-              <ErrorMessage name="password" component = {()=>(
-                <p className='text-red-500 font-semibold'>{errors.password}</p>
-              )}/>
-            </div>
-            {/* Repeat Password */}
-            <div className="my-5">
-              <label htmlFor="repeatPassword" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Repetir password</label>
-              <Field 
-                type="password" name="repeatPassword" id="repeatPassword" placeholder="Repite tu contraseña" 
-                className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-              />
-              <ErrorMessage name="repeatPassword" component = {()=>(
-                <p className='text-red-500 font-semibold'>{errors.repeatPassword}</p>
-              )}/>
-            </div>
-            <input type="submit" value="Iniciar sesión" className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5" />
-            {exit && <p className='text-emerald-500 font-semibold'>Formulario enviado con éxito</p>}
-          </Form>
-          // <form action="" onSubmit={handleSubmit} className="my-10 bg-white shadow rounded-lg px-10 py-5">
-          //   {/* Username */}
-          //   <div className="my-5">
-          //     <label htmlFor="username" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Username</label>
-          //     <input 
-          //       type="text" name="username" id="username" 
-          //       value={values.username}
-          //       placeholder="Username" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-          //       onChange={handleChange}
-          //       onBlur={handleBlur}
-          //     />
-          //     {touched.username && errors.username && <p className='text-red-500 font-semibold'>{errors.username}</p>}
-          //   </div>
-          //   {/* Email */}
-          //   <div className="my-5">
-          //     <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Email</label>
-          //     <input 
-          //       type="email" name="email" id="email" placeholder="Email de registro"
-          //       value={values.email}
-          //       className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-          //       onChange={handleChange}
-          //       onBlur={handleBlur}
-          //     />
-          //     {touched.email && errors.email && <p className='text-red-500 font-semibold'>{errors.email}</p>}
-          //   </div>
-          //   {/* Password */}
-          //   <div className="my-5">
-          //     <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Password</label>
-          //     <input 
-          //       type="password" name="password" id="password" placeholder="Ingresa tu contraseña password" 
-          //       value={values.password}
-          //       className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-          //       onChange={handleChange}
-          //       onBlur={handleBlur}
-          //     />
-          //     {touched.password && errors.password && <p className='text-red-500 font-semibold'>{errors.password}</p>}
-          //   </div>
-          //   {/* Repeat Password */}
-          //   <div className="my-5">
-          //     <label htmlFor="repeatPassword" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Repetir password</label>
-          //     <input 
-          //       type="password" name="repeatPassword" id="repeatPassword" placeholder="Repite tu contraseña" 
-          //       value={values.repeatPassword}
-          //       className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
-          //       onChange={handleChange}
-          //       onBlur={handleBlur}
-          //     />
-          //     {touched.repeatPassword && errors.repeatPassword && <p className='text-red-500 font-semibold'>{errors.repeatPassword}</p>}
-          //   </div>
-          //   <input type="submit" value="Iniciar sesión" className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5" />
-          //   {exit && <p className='text-emerald-500 font-semibold'>Formulario enviado con éxito</p>}
-          // </form>
+        {/* Username */}
+        <div className="my-5">
+          <label
+            htmlFor="username"
+            className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.username}
+          />
+          {formik.touched.username && formik.errors.username && (
+            <p className="text-red-500 font-semibold">
+              {formik.errors.username}
+            </p>
+          )}
+        </div>
+        {/* Email */}
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <p className="text-red-500 font-semibold">{formik.errors.email}</p>
+          )}
+        </div>
+        {/* Password */}
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Ingresa tu contraseña password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password && (
+            <p className="text-red-500 font-semibold">{formik.errors.password}</p>
+          )}
+        </div>
+        <input
+          type="submit"
+          value="Iniciar sesión"
+          className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5 disabled:bg-sky-950"
+        />
+        {exit && (
+          <p className="text-emerald-500 font-bold text-center">Formulario enviado con éxito</p>
         )}
-      </Formik>
+      </form>
       <nav className="lg:flex lg:justify-between">
         <Link to={"/"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold'>¿Ya tienes una cuenta? Inicia sesión</Link>
         <Link to={"/forget-password"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold'>¿Olvidaste tu contraseña?</Link>
---
+++ client/src/scheme/RegisterScheme.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+
+export const registerScheme = z.object({
+  username:z.string({
+    required_error:'El nombre de usuario es obligatorio'
+  }).min(5,{message:'El nombre de usuario debe tener al menos 5 caracteres'}).max(25,{message:'El nombre de usuario puede tener máximo 25 caracteres'}),
+  email: z.string({
+    required_error:'El email es obligatorio'
+  }).email({message:'Formato de email invalido'}),
+  password: z.string({
+    required_error:'La contraseña es obligatoria'
+  }).min(5,{message:'La contraseña debe tener al menos 5 caracteres'}).max(50,{message:'La contraseña puede tener máximo 50 caracteres'})
+})
