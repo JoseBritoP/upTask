548756b3a9a1936d818cf5bdfe4fec86787729fd
JoseBritoP <josealbritose@gmail.com>
2023-09-08 11:05:48 -0300

    .env front

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,7 +12,21 @@ import { router } from './routes';
 export const server = express();
 server.disable('x-powered-by');
 server.use(morgan('dev'));
-server.use(cors());
+server.use(cors({
+  origin: (origin,callback)=>{
+    const ACCEPTED_ORIGINS = [
+      `${process.env.FRONTEND}`
+    ]
+    if(origin && ACCEPTED_ORIGINS.includes(origin)){
+      return callback(null,true)
+    }
+    if(!origin){
+      return callback(null,true)
+    }
+
+    return callback(new Error('Not allowed by Cors'));
+  }
+}));
 server.use(express.json());
 // Conection
 server.use('/',router)
