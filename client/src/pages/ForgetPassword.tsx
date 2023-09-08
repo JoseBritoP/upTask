50fcb4ef99dcb9085d8947e3d5beb1e8788050b9
JoseBritoP <josealbritose@gmail.com>
2023-09-08 06:21:53 -0300

    Forget-Password darkmode

--- client/src/pages/ForgetPassword.tsx
+++ client/src/pages/ForgetPassword.tsx
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom'
 const ForgetPassword = () => {
   return (
     <>
-      <h1 className="text-sky-600 font-black text-4xl uppercase text-center">Recuperar el acceso de la <span className="text-slate-700">cuenta</span></h1>
-      <form action="" className="my-10 bg-white shadow rounded-lg px-10 py-5">
+      <h1 className="text-sky-600 font-black text-4xl uppercase text-center">Recuperar el acceso de la <span className="text-slate-700 dark:text-slate-300">cuenta</span></h1>
+      <form action="" className="my-10 bg-white shadow rounded-lg px-10 py-5 border-2 dark:bg-slate-950 dark:border-gray-700 dark:border-2">
         
         {/* Email */}
         <div className="my-5">
-          <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Email</label>
+          <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold dark:text-gray-200 hover:cursor-pointer">Email</label>
           <input type="email" name="email" id="email" placeholder="Ingrese la cuenta de email asociada" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" />
         </div>
-        <input type="submit" value="Enviar instrucciones" className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5" />
+        <input type="submit" value="Enviar instrucciones" className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer dark:bg-sky-800 dark:hover:bg-sky-700 transition-colors 300 ease-in mb-5 disabled:bg-sky-950" />
       </form>
       <nav className="lg:flex lg:justify-between">
-        <Link to={"/"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold'>¿Ya tienes una cuenta? Inicia sesión</Link>
-        <Link to={"/register"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold'>¿No tienes una cuenta? Regístrate</Link>
+        <Link to={"/"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold dark:text-gray-300'>¿Ya tienes una cuenta? Inicia sesión</Link>
+        <Link to={"/register"} className='block text-center my-5 text-slate-500 uppercase text-sm font-bold dark:text-gray-300'>¿No tienes una cuenta? Regístrate</Link>
       </nav>
     </>
   )
--- client/src/pages/NewPassword.tsx
+++ client/src/pages/NewPassword.tsx
@@ -32,36 +32,36 @@ const NewPassword = () => {
 
   return (
     <>
-      <h1 className="text-sky-600 font-black text-4xl uppercase text-center">Restablece tu <span className="text-slate-700">contraseña</span></h1>
-    <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+      <h1 className="text-sky-600 font-black text-4xl uppercase text-center">Restablece tu <span className="text-slate-700 dark:text-slate-300">contraseña</span></h1>
+    <form className="my-10 bg-white shadow rounded-lg px-10 py-5 border-2 dark:bg-slate-950 dark:border-gray-700 dark:border-2">
       {/* Password */}
       <div className="my-5">
-        <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Nueva Password</label>
+        <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold dark:text-gray-200 hover:cursor-pointer">Nueva Password</label>
 
-        <input type="password" name="password" id="password" placeholder="Ingresa tu nueva contraseña" className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black"
+        <input type="password" name="password" id="password" placeholder="Ingresa tu nueva contraseña"             className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black dark:border-gray-500 dark:bg-gray-900 dark:placeholder:text-gray-300"
          onChange={formik.handleChange}
          onBlur={formik.handleBlur}
          value={formik.values.password}
         />
         {formik.touched.password && formik.errors.password && (
-          <p className="text-red-500 font-semibold">
+          <p className="text-red-500 font-semibold pt-2 dark:text-red-600">
             {formik.errors.password}
           </p>
         )}
       </div>
       {/* Repeat Password */}
       <div className="my-5">
-        <label htmlFor="repeatPassword" className="uppercase text-gray-600 block text-xl font-bold hover:cursor-pointer">Confirmar Password</label>
+        <label htmlFor="repeatPassword" className="uppercase text-gray-600 block text-xl font-bold dark:text-gray-200 hover:cursor-pointer">Confirmar Password</label>
 
         <input 
           type="repeatPassword" name="repeatPassword" id="repeatPassword" placeholder="Confirma tu contraseña" 
-          className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black" 
+          className="w-full mt-3 p-3 border rounded-xl bg-gray-50 border-black dark:border-gray-500 dark:bg-gray-900 dark:placeholder:text-gray-300"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.repeatPassword}
         />
         {formik.touched.repeatPassword && formik.errors.repeatPassword && (
-          <p className="text-red-500 font-semibold">
+          <p className="text-red-500 font-semibold pt-2 dark:text-red-600">
             {formik.errors.repeatPassword}
           </p>
         )}
@@ -69,7 +69,7 @@ const NewPassword = () => {
       <input
         type="submit"
         value="Cambiar Contraseña"
-        className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer transition-colors 300 ease-in mb-5 disabled:bg-sky-950"
+        className="bg-sky-700 w-full py-3 text-white rounded uppercase font-bold hover:bg-sky-600 hover:cursor-pointer dark:bg-sky-800 dark:hover:bg-sky-700 transition-colors 300 ease-in mb-5 disabled:bg-sky-950"
       />
       {message && (
         <p className="text-emerald-500 font-bold text-center">Formulario enviado con éxito</p>
