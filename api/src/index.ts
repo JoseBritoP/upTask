ca76794e01fea4702c533aac7088bb74e7406e6f
JoseBritoP <josealbritose@gmail.com>
2023-09-08 14:43:29 -0300

    cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,21 +12,7 @@ import { router } from './routes';
 export const server = express();
 server.disable('x-powered-by');
 server.use(morgan('dev'));
-server.use(cors({
-  origin: (origin,callback)=>{
-    const ACCEPTED_ORIGINS = [
-      `${process.env.FRONTEND}`
-    ]
-    if(origin && ACCEPTED_ORIGINS.includes(origin)){
-      return callback(null,true)
-    }
-    if(!origin){
-      return callback(null,true)
-    }
-
-    return callback(new Error('Not allowed by Cors'));
-  }
-}));
+server.use(cors());
 server.use(express.json());
 // Conection
 server.use('/',router)
