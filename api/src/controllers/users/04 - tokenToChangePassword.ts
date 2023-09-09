8882901a344390aaebe1405a4c6925aa80710ff3
JoseBritoP <josealbritose@gmail.com>
2023-09-09 19:53:29 -0300

    Fix message error

--- api/src/controllers/users/04 - tokenToChangePassword.ts
+++ api/src/controllers/users/04 - tokenToChangePassword.ts
@@ -3,7 +3,7 @@ import { generateToken } from "../../utils/jwt.handler";
 
 export const tokenToChangePassword = async (email:any):Promise<String> => {
   const user = await User.findOne({email});
-  if(!user) throw new Error(`User not found`);
+  if(!user) throw new Error(`Usuario no encontrado`);
 
   const token = await generateToken(user.id);
   user.token = token;
