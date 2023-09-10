292f203a7e08727cfde8dfd3967f035a5f8fa4b4
JoseBritoP <josealbritose@gmail.com>
2023-09-10 04:35:38 -0300

    Fix newPassword

--- api/src/controllers/users/06 - newPassword.ts
+++ api/src/controllers/users/06 - newPassword.ts
@@ -2,7 +2,7 @@ import User from "../../models/User";
 import { bycrypt } from "../../utils/bycript.handler";
 import { ChangePassword,UserInterface } from "../../types/user";
 
-export const newPassword = async ({ token, password }: ChangePassword):Promise<String> => {
+export const newPassword = async ({ token, password }: ChangePassword):Promise<any> => {
     const user = await User.findOne({ token })
     .select("_id username email")
 
@@ -14,5 +14,7 @@ export const newPassword = async ({ token, password }: ChangePassword):Promise<S
     user.token = "";
     await user.save(); 
 
-    return `The password has been change successfully!`
+    return {
+        message: `La contraseña se ha actualizado correctamente!`
+    }
 };
