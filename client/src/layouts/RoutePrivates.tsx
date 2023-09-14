b23fc9f475521c46dd1b22867a2870b6e3d57c98
JoseBritoP <josealbritose@gmail.com>
2023-09-14 19:25:18 -0300

    Ruta privada

---
+++ client/src/layouts/RoutePrivates.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Outlet,Navigate } from "react-router-dom"
+import useProfile from "../hooks/useProfile"
+import useSession from "../hooks/useSession";
+
+const RoutePrivates = () => {
+  const {user, load} = useProfile();
+  const { id, username, email, proyects } = user;
+  const { session } = useSession();
+  console.log(session)
+  // console.log(load)
+  // console.log(user)
+  if(load) return 'Loading...'
+  return (
+    <div className="text-white">
+      {id ? 'Autenticado' : <Navigate to={'/'}/>}
+    </div>
+  )
+}
+
+export default RoutePrivates
---
+++ client/src/layouts/index.ts
@@ -0,0 +1,7 @@
+import AuthLayout from "./AuthLayout";
+import RoutePrivates from "./RoutePrivates";
+
+export {
+  AuthLayout,
+  RoutePrivates
+}
