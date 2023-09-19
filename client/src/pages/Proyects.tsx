9991154736d8c72f04a4be68a3e4c83eaccf91b9
JoseBritoP <josealbritose@gmail.com>
2023-09-19 03:00:12 -0300

    Renderizado de proyectos

--- client/src/pages/Proyects.tsx
+++ client/src/pages/Proyects.tsx
@@ -12,11 +12,14 @@ const Proyects = () => {
         ) : (
           <div className="flex flex-wrap gap-10 p-4">
             {proyects.map((proyect)=>(
-              <div className="flex flex-col flex-wrap border-2 gap-4 py-4 px-6 rounded-lg" key={proyect.id}>
-                 <h2>Nombre: {proyect.name}</h2>
-                 <h2>Descripción: {proyect.description}</h2>
-                 <h3>Fecha de entrega:{proyect.limitDate?.split('T')[0]}</h3>
-                 <p> Cliente: {proyect.client}</p>
+              <div className="flex flex-col flex-wrap border-2 gap-4 py-4 px-6 rounded-lg text-black dark:text-gray-50 bg-gray-100 dark:bg-transparent border-black dark:border-gray-400" key={proyect.id}>
+                 <h2 className="text-xl font-bold text-sky-600">Nombre: <span className="text-black dark:text-gray-100 font-semibold">{proyect.name}</span></h2>
+                 <div>
+                 <h2 className="text-xl font-bold text-sky-600">Descripción</h2>
+                  <p className="text-black dark:text-gray-100 font-semibold">{proyect.description}</p>
+                 </div>
+                 <h3 className="text-xl font-bold text-sky-600">Fecha de entrega: <span className="text-black dark:text-gray-100 font-semibold">{proyect.limitDate?.split('T')[0]}</span></h3>
+                 <h3 className="text-xl font-bold text-sky-600">Cliente: <span className="text-black dark:text-gray-100 font-semibold">{proyect.client}</span></h3>
               </div>
             ))}
           </div>
